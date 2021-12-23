04672692e9073902cdfbfbbf3d38711e216b3eb5
Cenon Ebora <remote.cenon@gmail.com>
2021-12-24 01:05:36 +0800

    lcd sketch - updated code

--- LcdDisplay/LcdDisplay.ino
+++ LcdDisplay/LcdDisplay.ino
@@ -68,21 +68,20 @@ void setup()
   
   // Print on first row
   lcd.setCursor(0,0);
-  lcd.print("Hello world!");
+  lcd.print("Dinno's world!");
   
   // Wait 1 second
   delay(1000);
   
   // Print on second row
   lcd.setCursor(0,1);
-  lcd.print("How are you?");
+  lcd.print("Enjoy the ride");
   
   // Wait 8 seconds
-  delay(8000);
+  delay(2000);
   
   // Clear the display
   lcd.clear();
-
 }
 
 
@@ -90,37 +89,37 @@ void loop()
 {
   
   // Demo 1 - flash backlight
-  lcd.setCursor(0,0);
-  lcd.print("Backlight demo");
-  lcd.setCursor(0,1);
-  lcd.print("Flash 4 times");
-  
-  delay(3000);
-  lcd.clear();
-  
-  // Flash backlight 4 times
-  for(int i = 0; i< 4; i++)
-    {
-    lcd.backlight();
-    delay(250);
-    lcd.noBacklight();
-    delay(250);
-    }
-
-  // Turn backlight back on
-  lcd.backlight();
+//  lcd.setCursor(0,0);
+//  lcd.print("Backlight demo");
+//  lcd.setCursor(0,1);
+//  lcd.print("Flash 4 times");
+//  
+//  delay(3000);
+//  lcd.clear();
+//  
+//  // Flash backlight 4 times
+//  for(int i = 0; i< 4; i++)
+//    {
+//    lcd.backlight();
+//    delay(250);
+//    lcd.noBacklight();
+//    delay(250);
+//    }
+//
+//  // Turn backlight back on
+//  lcd.backlight();
     
   // Demo 2 - scroll
   lcd.setCursor(0,0);
-  lcd.print("Scroll demo - ");
-  delay(1500);
-  // set the display to automatically scroll:
+  lcd.print("Announcement!!!");
+  lcd.setCursor(0,1);
   lcd.autoscroll();
-  // print from 0 to 9:
-  for (int thisChar = 0; thisChar < 10; thisChar++) {
-    lcd.print(thisChar);
-    delay(500);
-    }
+  
+  // print string
+  for (char x  : "Everything is awesome!!! everything is cool!!!") {
+    lcd.print(x);
+    delay(300);
+  }
   // turn off automatic scrolling
   lcd.noAutoscroll();
 
