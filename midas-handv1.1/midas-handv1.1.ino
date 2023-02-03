2bc9a9f0078c7950ec48a9bd36945517ca061393
Cenon Ebora <cenon4dno@gmail.com>
2023-02-03 22:06:48 +0800

    Multi Servo code

--- midas-handv1.1/midas-handv1.1.ino
+++ midas-handv1.1/midas-handv1.1.ino
@@ -22,7 +22,7 @@ const int POTENTIOMETER_PIN4   = A3;
 const int POTENTIOMETER_PIN5   = A4;
 const int POTENTIOMETER_PIN6   = A5;
 // constants for servo
-const int SERVO_PIN1           = 6; // Arduino pin connected to Servo Motor's pin
+const int SERVO_PIN1           = 3; // Arduino pin connected to Servo Motor's pin
 const int SERVO_PIN2           = 4;
 const int SERVO_PIN3           = 5;
 const int SERVO_PIN4           = 6;
@@ -31,6 +31,12 @@ const int SERVO_PIN6           = 8;
 //
 int analogPrevValue = 0;
 int servoPosition = 0;
+int servoPosition1 = 0;
+int servoPosition2 = 0;
+int servoPosition3 = 0;
+int servoPosition4 = 0;
+int servoPosition5 = 0;
+int servoPosition6 = 0;
 int servoConstant1 = 7.5; // for servo 1 - 7.5, servo 2 = 7.5, , servo 3 = 7.5, , servo 4 = 7.5, , servo 5 = 3, , servo 6 = 7.5
 int servoConstant2 = 7.5;
 int servoConstant3 = 7.5;
@@ -39,49 +45,78 @@ int servoConstant5 = 3;
 int servoConstant6 = 3;
 
 
-Servo servo; // create servo object to control a servo
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+Servo servo6; // create servo object to control a servo
 
 void setup() {
   Serial.begin(9600); // Initialize UART with 9600 Baud rate
-  servo.attach(SERVO_PIN1);   // attaches the servo on pin 9 to the servo object
-  servo.write(servoPosition);
+  Serial.println("Boot up");
+  servo1.attach(SERVO_PIN1);
+  servo1.write(servoPosition1);
+  servo2.attach(SERVO_PIN2);
+  servo2.write(servoPosition2);
+  servo3.attach(SERVO_PIN3);
+  servo3.write(servoPosition3);
+  // servo4.attach(SERVO_PIN4);
+  // servo4.write(servoPosition);
+  // servo5.attach(SERVO_PIN5);
+  // servo5.write(servoPosition);
+  // servo6.attach(SERVO_PIN6);
+  // servo6.write(servoPosition);
 }
 
 void loop() {
   // Check potentionmeter1
   moveServo(checkPotentiometer(POTENTIOMETER_PIN1), servoConstant1, 1);
+  moveServo(checkPotentiometer(POTENTIOMETER_PIN2), servoConstant2, 2);
+  moveServo(checkPotentiometer(POTENTIOMETER_PIN3), servoConstant3, 3);
+  // moveServo(checkPotentiometer(POTENTIOMETER_PIN4), servoConstant4, 4);
+  // moveServo(checkPotentiometer(POTENTIOMETER_PIN5), servoConstant5, 5);
+  // moveServo(checkPotentiometer(POTENTIOMETER_PIN6), servoConstant6, 6);
 }
 
 int checkPotentiometer(int potentionmeter) {
-  return analogRead(potentionmeter);      // read the input on analog pin
+  int analog = analogRead(potentionmeter);      // read the input on analog pin
+  Serial.println("New " + String(potentionmeter) +" position: " + String(analog));
+  return analog;
 }
 
 void moveServo(int analogValue, int servoConstant, int servoId) {
-    int newPosition = servoPosition + (analogValue/servoConstant);
+    int newPosition = 0;
     switch(servoId) {
       case 1: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition1 + (analogValue/servoConstant);
+        servo1.write(newPosition);
+        //Serial.println("New servo1 position: " + String(newPosition));
         break;
        case 2: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition2 + (analogValue/servoConstant);
+        servo2.write(newPosition);
+        //Serial.println("New servo2 position: " + String(newPosition));
         break;
       case 3: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition3 + (analogValue/servoConstant);
+        servo3.write(newPosition);
+        //Serial.println("New servo3 position: " + String(newPosition));
         break;
       case 4: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition4 + (analogValue/servoConstant);
+        servo4.write(newPosition);
+        Serial.println("New servo4 position: " + String(newPosition));
         break;
       case 5: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition5 + (analogValue/servoConstant);
+        servo5.write(newPosition);
+        Serial.println("New servo5 position: " + String(newPosition));
         break;
       case 6: 
-        servo.write(newPosition);
-        Serial.println("New servo1 position: " + String(newPosition));
+        newPosition = servoPosition6 + (analogValue/servoConstant);
+        servo6.write(newPosition);
+        Serial.println("New servo6 position: " + String(newPosition));
         break;
     }
 }
